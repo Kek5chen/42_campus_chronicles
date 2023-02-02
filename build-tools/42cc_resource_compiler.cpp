13c86164d0097a94b5d1e7ba89d1034b9d9c40f6
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2023-02-02 15:08:45 +0100

    feat: added resource check

--- build-tools/42cc_resource_compiler.cpp
+++ build-tools/42cc_resource_compiler.cpp
@@ -23,23 +23,17 @@ std::streamsize read_infile(const std::string& filename, std::vector<char> &data
 	return size;
 }
 
-int append_to_outfile(const std::string& filename,
+void append_to_outfile(std::ofstream &outfile,
 					  long long fileSize,
 					  const std::string &path,
 					  const std::vector<char> &data)
 {
-	std::ofstream	outfile(filename, std::ios::app | std::ios::binary);
-
-	if(!outfile.is_open())
-		return -1;
 	outfile.write((char*)&fileSize, 8);
 	outfile.write("\0", 1);
 	outfile << path;
 	outfile.write("\0", 1);
 	outfile.write(data.data(), fileSize);
 	outfile.write("\0", 1);
-	outfile.close();
-	return 0;
 }
 
 int	main(int argc, char **argv)
@@ -52,6 +46,12 @@ int	main(int argc, char **argv)
 		printf("Usage: %s <infile> <resfile>\n", argv[0]);
 		return 1;
 	}
+	std::ofstream	outfile(argv[argc - 1], std::ios::binary | std::ios_base::trunc);
+	if (!outfile.is_open()) {
+		printf("Error: Could not open output file %s\n", argv[argc - 1]);
+		return 1;
+	}
+
 	i = 1;
 	while (i < argc - 1) {
 		printf("Input: %s\nOutput: %s\n", argv[i], argv[argc - 1]);
@@ -60,13 +60,10 @@ int	main(int argc, char **argv)
 			printf("Error: Could not open input file %s\n", argv[i]);
 			return 1;
 		}
-		fileSize = append_to_outfile(argv[argc - 1], fileSize, argv[i], infileData);
-		if (fileSize < 0) {
-			printf("Error: Could not open output file %s\n", argv[argc - 1]);
-			return 1;
-		}
+		append_to_outfile(outfile, fileSize, argv[i], infileData);
 		infileData.clear();
 		i++;
 	}
+	outfile.close();
 	return 0;
 }
