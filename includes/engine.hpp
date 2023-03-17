fefae524edbbd1fae0841ab7d786219475f03283
kx <kx@imkx.dev>
2023-03-17 01:08:48 +0100

    feat: expanded engine base objects

--- includes/engine.hpp
+++ includes/engine.hpp
@@ -1,14 +1,59 @@
 #pragma once
 
+struct Matrix2 {
+};
+
 struct Vector2 {
-	float x = 0;
-	float y = 0;
+	Vector2() = default;
+	Vector2(float x, float y);
+
+	Vector2 		operator+(const Vector2& other) const;
+	Vector2 		operator-(const Vector2& other) const;
+	Vector2 		operator*(const Vector2& other) const;
+	Vector2 		operator/(const Vector2& other) const;
+	float&			operator[](int index);
+	const float&	operator[](int index) const;
+
+	float x, y;
 };
 
 struct Vector3 {
-	float 				x = 0;
-	float 				y = 0;
-	float				z = 0;
+	Vector3() = default;
+	Vector3(float x, float y, float z);
+
+	Vector3 		operator+(const Vector3& other) const;
+	Vector3 		operator-(const Vector3& other) const;
+	Vector3 		operator*(const Vector3& other) const;
+	Vector3 		operator/(const Vector3& other) const;
+	float&			operator[](int index);
+	const float&	operator[](int index) const;
+
+	float x, y, z;
+};
+
+struct Vector4 {
+	Vector4() = default;
+	Vector4(float x, float y, float z, float w);
+
+	Vector4 		operator+(const Vector4& other) const;
+	Vector4 		operator-(const Vector4& other) const;
+	Vector4 		operator*(const Vector4& other) const;
+	Vector4 		operator/(const Vector4& other) const;
+	float&			operator[](int index);
+	const float&	operator[](int index) const;
+
+	float x, y, z, w;
+};
+
+struct Matrix4 {
+	Matrix4();
+	Matrix4(Vector4 v0, Vector4 v1, Vector4 v2, Vector4 v3);
+
+	Vector4&		operator[](int index);
+	const Vector4&	operator[](int index) const;
+	Matrix4			operator*(const Matrix4& other) const;
+
+	Vector4	v[4];
 };
 
 struct Object {
@@ -22,11 +67,9 @@ struct Object {
 
 class Camera {
 public:
-	Vector3 pos;
+	Vector3	pos;
 };
 
 struct Triangle3 {
-	Vector3 a;
-	Vector3 b;
-	Vector3 c;
+	Vector3	v[3];
 };
---
+++ src/engine/engine_base_objects.cpp
@@ -0,0 +1,136 @@
+#include "engine.hpp"
+
+
+/*
+ * Vector 2 Implementations
+ */
+
+
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+Vector2 Vector2::operator+(const Vector2& other) const {
+	return Vector2(this->x + other.x, this->y + other.y);
+}
+
+Vector2 Vector2::operator-(const Vector2& other) const {
+	return Vector2(this->x- other.x, this->y - other.y);
+}
+
+Vector2 Vector2::operator*(const Vector2& other) const {
+	return Vector2(this->x * other.x, this->x * other.x);
+}
+
+Vector2 Vector2::operator/(const Vector2& other) const {
+	return Vector2(this->x / other.x, this->y / other.y);
+}
+
+float& Vector2::operator[](int index) {
+    return (&x)[index];
+}
+
+const float& Vector2::operator[](int index) const {
+    return (&x)[index];
+}
+
+
+/*
+ * Vector 3 Implementations
+ */
+
+
+Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+Vector3 Vector3::operator+(const Vector3 &other) const {
+	return Vector3(this->x + other.x, this->y + other.y, this->z + other.z);
+}
+
+Vector3 Vector3::operator-(const Vector3 &other) const {
+	return Vector3(this->x - other.x, this->y - other.y, this->z - other.z);
+}
+
+Vector3 Vector3::operator*(const Vector3 &other) const {
+	return Vector3(this->x * other.x, this->y * other.y, this->z * other.z);
+}
+
+Vector3 Vector3::operator/(const Vector3 &other) const {
+	return Vector3(this->x / other.x, this->y / other.y, this->z / other.z);
+}
+
+float& Vector3::operator[](int index) {
+    return (&x)[index];
+}
+
+const float& Vector3::operator[](int index) const {
+    return (&x)[index];
+}
+
+
+/*
+ * Vector 4 Implementations
+ */
+
+
+Vector4::Vector4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+Vector4 Vector4::operator+(const Vector4 &other) const {
+	return Vector4(this->x + other.x, this->y + other.y, this->z + other.z, this->w + other.w);
+}
+
+Vector4 Vector4::operator-(const Vector4 &other) const {
+	return Vector4(this->x - other.x, this->y - other.y, this->z - other.z, this->w - other.w);
+}
+
+Vector4 Vector4::operator*(const Vector4 &other) const {
+	return Vector4(this->x * other.x, this->y * other.y, this->z * other.z, this->w * other.w);
+}
+
+Vector4 Vector4::operator/(const Vector4 &other) const {
+	return Vector4(this->x / other.x, this->y / other.y, this->z / other.z, this->w / other.w);
+}
+
+float& Vector4::operator[](int index) {
+    return (&x)[index];
+}
+
+const float &Vector4::operator[](int index) const {
+    return (&x)[index];
+}
+
+
+/*
+ * Matrix 4 Implementations
+ */
+
+
+Matrix4::Matrix4() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            v[i][j] = 0;
+        }
+    }
+}
+
+Matrix4::Matrix4(Vector4 v0, Vector4 v1, Vector4 v2, Vector4 v3) : v{v0, v1, v2, v3} {}
+
+
+Vector4& Matrix4::operator[](int index) {
+    return v[index];
+}
+
+const Vector4 &Matrix4::operator[](int index) const {
+    return v[index];
+}
+
+
+Matrix4 Matrix4::operator*(const Matrix4 &other) const {
+    Matrix4 result;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < 4; k++) {
+                result[i][j] += v[i][k] * other[k][j];
+            }
+        }
+    }
+    return result;
+}
