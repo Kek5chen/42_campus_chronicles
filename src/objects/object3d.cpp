18bdb25db812fa340be06d587fbf2f0cf65468ce
kx <kx@imkx.dev>
2023-03-17 14:23:50 +0100

    feat: added proper faces with simple shading

--- src/objects/object3d.cpp
+++ src/objects/object3d.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "objects/objects.hpp"
 #include "game.hpp"
 #include "engine/math.hpp"
@@ -47,16 +48,56 @@ void Object3D::draw() {
 	worldProjection = math::translate(worldProjection, this->_game->camera.pos);
 	projectionMatrix = worldProjection * projectionMatrix;
 
+	std::vector<Triangle3> projectedTriangles;
     for (const Triangle3& tri : _triangles) {
-        Triangle3 projectedTriangle = tri;
-		for (auto & i : projectedTriangle.v) {
+		Triangle3 projectedTriangle = tri;
+		for (auto &i: projectedTriangle.v) {
 			Vector4 transformedVertex = modelMatrix * Vector4(i, 1.0f);
-			i = math::point_to_screen((Vector3&)transformedVertex, projectionMatrix, screenSize);
+			i = math::point_to_screen((Vector3 &) transformedVertex, projectionMatrix, screenSize);
+		}
+		projectedTriangle.normals[0] = tri.normals[0];
+		projectedTriangle.normals[1] = tri.normals[1];
+		projectedTriangle.normals[2] = tri.normals[2];
+		projectedTriangles.push_back(projectedTriangle);
+	}
+	std::sort(projectedTriangles.begin(), projectedTriangles.end(), [](const Triangle3& a, const Triangle3& b) {
+		return a.v->z > b.v->z;
+	});
+
+	for (const Triangle3& tri : projectedTriangles) {
+		Vector2 min_coords = tri.v[0];
+		Vector2 max_coords = tri.v[0];
+
+		for (int i = 1; i < 3; ++i) {
+			min_coords.x = std::min(min_coords.x, tri.v[i].x);
+			min_coords.y = std::min(min_coords.y, tri.v[i].y);
+			max_coords.x = std::max(max_coords.x, tri.v[i].x);
+			max_coords.y = std::max(max_coords.y, tri.v[i].y);
 		}
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderLine(renderer, projectedTriangle.v[0].x, projectedTriangle.v[0].y, projectedTriangle.v[1].x, projectedTriangle.v[1].y);
-        SDL_RenderLine(renderer, projectedTriangle.v[1].x, projectedTriangle.v[1].y, projectedTriangle.v[2].x, projectedTriangle.v[2].y);
-        SDL_RenderLine(renderer, projectedTriangle.v[2].x, projectedTriangle.v[2].y, projectedTriangle.v[0].x, projectedTriangle.v[0].y);
+		Vector2 triangleScreenSize;
+		triangleScreenSize.x = max_coords.x - min_coords.x;
+		triangleScreenSize.y = max_coords.y - min_coords.y;
+		if (triangleScreenSize.x < 1 || triangleScreenSize.y < 1)
+			continue;
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		Vector3 centroid{};
+		for (int i = 0; i < 3; ++i)
+			centroid += tri.normals[i];
+		centroid /= 3.0f;
+		centroid = centroid.normalize();
+		SDL_Vertex vertices[3];
+		int gradient = (int)((centroid.y < 0 ? 0 : centroid.y) * 150 + 105);
+		for (int i = 0; i < 3; ++i) {
+			vertices[i].position.x = tri.v[i].x;
+			vertices[i].position.y = tri.v[i].y;
+			vertices[i].color.r = gradient;
+			vertices[i].color.g = gradient;
+			vertices[i].color.b = gradient;
+			vertices[i].color.a = 255;
+		}
+		int indices[3] = {2, 1, 0};
+		SDL_RenderGeometry(renderer, 0, vertices, 3, indices, 3);
     }
 }
