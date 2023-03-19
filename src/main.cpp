b0fa567fa0a35249e70dfa5263e8f6620431f083
kx <kx@imkx.dev>
2023-03-19 22:52:36 +0100

    feat: added global exception handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <sstream>
 #include "game.hpp"
-
+#include "engine/exceptions/stackexception.hpp"
+#include <stacktrace>
 
 /* Sets constants */
 // #define WIDTH 800
@@ -8,7 +11,9 @@
 
 int main (int argc, char **argv)
 {
-	Game new_game;
-	new_game.loop();
+	GLOBAL_EXCEPT_TRY
+		Game new_game;
+		new_game.loop();
+	GLOBAL_EXCEPT_CATCH
 	return 0;
 }
