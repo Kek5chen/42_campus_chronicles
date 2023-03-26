e57b092fc1f5217686ad45fdecd43dded8337aef
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2023-03-26 19:35:13 +0200

    refactor: move defines below includes

--- includes/includes.hpp
+++ includes/includes.hpp
@@ -1,9 +1,4 @@
 #pragma once
-
-#define TITLE "42 Campus Chronicles"
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
 #ifdef _WIN32
 # include <SDL3/SDL.h>
 # include <SDL3/SDL_image.h>
@@ -12,7 +7,10 @@
 # include <SDL_image.h>
 #include <memory>
 
-#endif
+#define TITLE "42 Campus Chronicles"
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 
 using SurfacePtr = std::shared_ptr<SDL_Surface>;
 using TexturePtr = std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)>;
